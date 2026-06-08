af0d8c5a7aeb57140ede352b36e47de6fc5785f7
Cebisile16 <ngubsceby@gmail.com>
2026-06-08 10:19:23 +0000

    first attempt

--- src/main/java/com/shop/OrderStatus.java
+++ src/main/java/com/shop/OrderStatus.java
@@ -3,5 +3,9 @@ package com.shop;
 public enum OrderStatus {
 
     // TODO
+    PENDING,
+    PAID,
+    SHIPPED,
+    CANCELLED,
 
 }
--- src/main/java/com/shop/Product.java
+++ src/main/java/com/shop/Product.java
@@ -3,5 +3,49 @@ package com.shop;
 public class Product {
 
     // TODO
+    private String productId;
+    private String name;
+    private double price;
 
+    public Product(String productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        if (productId ==null){
+            throw new IllegalArgumentException(" ");
+        }
+        if (name == null){
+            throw new IllegalArgumentException("");
+        }
+        if (price <=0){
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+    }
+
+    public String productId() {
+        return productId;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public double price() {
+        return price;
+    }
+    public void definePrice(double price){
+        if (price <=0){
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
